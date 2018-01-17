ebfef7a6252dcdbe5496167c4f4bbe6fcd7129fa
Richard Red <richardred.q@gmail.com>
2018-01-17 00:55:22 -0500

    test

--- maplistmaker.py
+++ maplistmaker.py
@@ -6,14 +6,16 @@ from tkinter import *
 root = tk.Tk()
 root.withdraw()
 
-filepath = filedialog.askdirectory()
-print(filepath)
+filepath = ''
 
-#filepath = os.getenv('LOCALAPPDATA') + '\\osu!\\Songs'
+try:
+    filepath = os.getenv('LOCALAPPDATA') + '\\osu!\\Songs'
+except Exception:
+    filepath = filedialog.askdirectory()
 
 map_list = [dI for dI in os.listdir(filepath) if os.path.isdir(os.path.join(filepath,dI))]
-
 url_list = []
+
 for map_dir in map_list:
     tokens = map_dir.split()
     url_list.append('https://osu.ppy.sh/s/' + tokens[0])
@@ -21,11 +23,9 @@ for map_dir in map_list:
 def redirect_to_file():
     desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
     os.chdir(desktop)
-    print(os.getcwd())
     with open('beatmaplist.txt', 'w', encoding='utf-8') as outfile:
         for item in url_list:
             outfile.write('%s\n' % item)
 
 if __name__ == '__main__':
-    #redirect_to_file()
-    print()
+    redirect_to_file()
